6473d061e36f10f7456e7e40254ac7f3c4831a10
Muhammad Talha <talhaofficialwork@gmail.com>
2021-12-06 22:13:49 +0500

    Basic Prototype Design

---
+++ src/pages/index.tsx
@@ -0,0 +1,38 @@
+import * as React from "react"
+import Todos from '../components/Todos'
+import { makeStyles, Theme, createStyles } from '@material-ui/core/styles';
+
+
+
+const baseURL = ".netlify/functions/faunadb-crud/";
+
+const useStyles = makeStyles((theme: Theme) =>
+    createStyles({
+        root: {
+            width: '100%',
+            textAlign:'center',
+        },
+        parent:{
+          textAlign:'center'
+        }
+    }),
+);
+
+
+// markup
+const IndexPage = () => {
+
+  const classes=useStyles();
+
+  return (
+    <div className={classes.parent}>
+      <title>Todos</title>
+      <h1>Todos</h1>
+      {/* <AddTodo/> */}
+      <Todos/>
+  
+    </div>
+  )
+}
+
+export default IndexPage
